9e03d797832c9e63a8278fcfdb1587289d1e8bfd
sggnology <sggnology@gmail.com>
2025-10-03 18:10:27 +0900

    [github-page] github page 를 github action 으로 배포하기 위한 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/test-github-page' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/test-github-page' : '',
+};
 
 export default nextConfig;
